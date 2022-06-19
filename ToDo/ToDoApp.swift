aa022936c40fc9a22276498f0370562695ba8ebc
Joshua David Ang <joshuadavidang@outlook.sg>
2022-06-20 07:34:32 +0800

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Joshua David Ang on 20/6/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
